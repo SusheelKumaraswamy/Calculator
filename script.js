d2ea49ec341adf2b53942552a63de820dceae30f
SusheelKumaraswamy <93818386+SusheelKumaraswamy@users.noreply.github.com>
2022-01-26 12:35:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 var input1=document.getElementById("input1");
 input1.addEventListener("keypress",function(e){
-  if(!(e.keyCode>=48 && e.keyCode<=57)){
+  if (e.keyCode != 46 && e.keyCode > 31 && (e.keyCode < 48 || e.keyCode > 57)){
     alert("Warning:Enter Number alone");
+    e.preventDefault();
+  }
+  if(e.keyCode===46){
+    count++;
+  }
+  if(count>1 && e.keyCode===46){
     e.preventDefault();
   }
 })
 
 var input2=document.getElementById("input2");
+var count=0;
 input2.addEventListener("keypress",function(e){
-  if(!(e.keyCode>=48 && e.keyCode<=57)){
+  
+  if (e.keyCode != 46 && (e.keyCode < 48 || e.keyCode > 57)){
     alert("Warning:Enter Number alone");
+    e.preventDefault();
+  }
+  if(e.keyCode===46){
+    count++;
+  }
+  if(count>1 && e.keyCode===46){
     e.preventDefault();
   }
 })
 
 var equals=document.getElementById("equals");
 equals.addEventListener("click",function(){
-  var a=parseInt(document.getElementById("input1").value);
-  var b=parseInt(document.getElementById("input2").value);
+  var a=parseFloat(document.getElementById("input1").value);
+  console.log(a);
+  var b=parseFloat(document.getElementById("input2").value);
   var symbols=document.getElementById("values").value;
   
   if(symbols==="+"){
